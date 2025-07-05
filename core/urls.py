a03a7ee2cde047d0579f0f3c54d0a87e909ba0e7
Luan Alves <luancristianrodriguesalves@gmail.com>
2025-07-05 16:32:27 -0300

    Remover arquivos ignorados após adicionar .gitignore

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='core'
-
-urlpatterns = [
-    path('', views.HomeView, name='homeview' ),
-    path('dashboard/', views.DashBoard, name="dashboardview"),
-]
